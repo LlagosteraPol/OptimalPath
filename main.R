5b7fef12bf0e1ca59b99673d75867623efe7f3dd
Pol Llagostera Blasco <plb92@hotmail.com>
2022-04-10 12:56:09 +0200

    Added IMD data to old DB and assembled into a new dataframe (also saved in a .csv file)

---
+++ main.R
@@ -0,0 +1,40 @@
+
+
+source("DB_preparation.R")
+source("plots.R")
+
+# Add weights to the edges matrix
+weighted_segments <- cbind(Dades_segments, Dades_pesos) 
+weighted_segments <- cbind(weighted_segments, Dades_distancies)
+weighted_segments <- cbind(weighted_segments, transformed_accIntensities) 
+weighted_segments <- cbind(weighted_segments, transformed_distances)
+weighted_segments <- cbind(weighted_segments, all_data)
+
+colnames(weighted_segments) <- c("from","to","weight","distance", "t_weight", "t_distance", "all")
+
+
+vertices_df <- data.frame(names = seq(1, length(Dades_vertex[[1]]), 1), 
+                          xcoord = Dades_vertex$V1, 
+                          ycoord = Dades_vertex$V2)
+
+segments_df <- data.frame(from = weighted_segments$from,
+                          to = weighted_segments$to,
+                          weight = weighted_segments$weight,
+                          imd = imd,
+                          distance = weighted_segments$distance,
+                          t_weight = weighted_segments$t_weight,
+                          t_distance = weighted_segments$t_distance,
+                          all = weighted_segments$all)
+
+g = graph_from_data_frame(segments_df, directed=FALSE, vertices=vertices_df)
+
+PlotNetwork(g, net_edges = igraph::E(g)[1], high_size = 10)
+PlotNetwork(g, net_nodes = c(1,2), net_edges = igraph::E(g)[1], high_size = 10)
+PlotNetwork(g, net_vertices = c(igraph::ends(g, igraph::E(g)[2])), net_edges = igraph::E(g)[2])
+
+
+checker <- function(g, i){
+  PlotNetwork(g, net_vertices = c(igraph::ends(g, igraph::E(g)[i])), net_edges = igraph::E(g)[i])
+}
+
+
